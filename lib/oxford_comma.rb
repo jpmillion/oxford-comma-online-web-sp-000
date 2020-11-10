85ada1a2075235b0e8299252a2a7946e34b39a33
John Million <jpmillion@comcast.net>
2020-11-10 00:42:17 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,19 +1,15 @@
 def oxford_comma(array)
   case array.count
-  
-  #when 1 
-    #return array.join
     
   when 2 
     return array.join(" and ")
     
   when 3..1000000
-    #return array.slice(0, array.count - 1).join(", ") + ", and " + array[-1]
     array[-1].insert(0, "and ")
-    #array.join(", ")
   end
   
   return array.join(", ")
+  
 end
 
 
